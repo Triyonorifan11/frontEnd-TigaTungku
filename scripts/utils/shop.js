230b2b6a4431ce9e55639679b9eb2c7f5e18d6b0
Triyono Rifan <triyonorifan01@gmail.com>
2022-12-13 13:01:13 +0700

    update search produk

--- scripts/utils/shop.js
+++ scripts/utils/shop.js
@@ -1,7 +1,19 @@
 $(document).ready(function () {
     const rowDaftarProduk = document.querySelector('#daftar-produk');
+    const searchProduk = document.querySelector('#searchProduk');
+    const formSearch = document.querySelector('#formSearch');
+
+    function escapeHtml(text) {
+        return text
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#039;');
+    }
+
     const fetchAllProduk = async () => {
-        const q = query(collection(db, 'products'));
+        let q = query(collection(db, 'products'), orderBy('nama_produk'));
         const snap = await getDocs(q);
         rowDaftarProduk.innerHTML = '';
         snap.forEach((doc) => {
@@ -34,6 +46,52 @@ $(document).ready(function () {
     }
     fetchAllProduk();
 
+    const btnRefresh = document.getElementById('btnRefresh');
+    formSearch.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        btnRefresh.classList.remove('d-none')
+        const dataInputSearch = escapeHtml(searchProduk.value);
+        console.log(dataInputSearch);
+        let q = query(collection(db, 'products'), orderBy('nama_produk'), where('nama_produk', '<=', dataInputSearch))
+        const snapDoc = await getDocs(q);
+        rowDaftarProduk.innerHTML = 'Search..';
+        rowDaftarProduk.innerHTML = '';
+        snapDoc.forEach((doc) => {
+            const data = doc.data();
+            if (data) {
+                console.log(data);
+                data.idProduk = doc.id;
+                rowDaftarProduk.innerHTML += `
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="product__item">
+                    <div class="product__item__pic set-bg" data-setbg="${data.foto_produk}">
+                        <div class="product__label">
+                            <span id="namaProduk">${data.nama_produk}</span>
+                        </div>
+                    </div>
+                    <div class="product__item__text">
+                        <h6 data-namaProduk="${data.nama_produk}"><a href="${base_url('shop-details', '?produk=' + data.idProduk)}">${data.nama_produk}</a></h6>
+                        <div class="product__item__price">Rp${formatRupiah(data.harga_produk)}</div>
+                        <div class="cart_add">
+                            <a href="${data.linkShopee_produk}" target="_blank">Link Shopee</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
+            }else{
+                console.log('tidak ditemukqn')
+            }
+        });
 
+        $('.set-bg').each(function () {
+            let bg = $(this).data('setbg');
+            $(this).css('background-image', 'url(' + bg + ')');
+        });
+    })
+
+    btnRefresh.addEventListener('click', (e)=>{
+        window.location.reload();
+    })
 
 })
